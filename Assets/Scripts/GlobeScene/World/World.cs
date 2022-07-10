1a4018e85098262c4ac87ab2fe379e67179f1a38
justin_k_m <justinm0623@gmail.com>
2022-07-10 02:23:14 -0400

    Update World.cs

--- Assets/Scripts/GlobeScene/World/World.cs
+++ Assets/Scripts/GlobeScene/World/World.cs
@@ -1,21 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+using IDebug;
+using Generation.Voronoi;
 
 public class World : MonoBehaviour
 {
+    [Header("Log Time")]
+    [Tooltip("When enabled, will output the times for the given script.")]
+    [SerializeField] private bool logWorld;
+    [SerializeField] private bool logVoronoiSphere;
     [Header("VoronoiSphere")]
     [Tooltip("Specifies the resolution of the voronoi sphere initial vertices.")]
     [SerializeField] [Range(2,256)] private int resolution = 2;
-    [Tooltip("Adds randomization in points after voronoi cells are calculated")]
+    [Tooltip("Adds randomization in points after voronoi cells are calculated.")]
     [SerializeField] [Range(0f, 1f)] private float jitter = 0f;
 
-    private VoronoiSphere voronoiSphere;
+    private Sphere voronoiSphere;
+    private TimeTracker debugTimeTracker;
 
     private void Start()
     {
-        voronoiSphere = new VoronoiSphere(resolution, jitter);
+        debugTimeTracker = (logWorld) ? new TimeTracker() : null;
+        voronoiSphere = new Sphere(resolution, jitter, logVoronoiSphere);
 
-        voronoiSphere.VoronoiSphereMesh.transform.parent = this.transform;
+        voronoiSphere.CubeSphereMesh.transform.parent = this.transform;
+        // voronoiSphere.VoronoiSphereMesh.transform.parent = this.transform;
     }
 }
