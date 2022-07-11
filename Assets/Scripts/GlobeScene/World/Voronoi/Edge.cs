3587be3979b0bbb5bfe00c89dc1554f55f2453b5
justin_k_m <justinm0623@gmail.com>
2022-07-11 01:30:52 -0400

    Update Edge.cs
    Moved Comparer

--- Assets/Scripts/GlobeScene/World/Voronoi/Edge.cs
+++ Assets/Scripts/GlobeScene/World/Voronoi/Edge.cs
@@ -7,10 +7,13 @@ namespace Generation.Voronoi {
         public Vector3 A { get; private set; }
         public Vector3 B { get; private set; }
 
+        public Vector3 Midpoint { get; private set; }
         public Edge(Vector3 a, Vector3 b)
         {
             A = a;
             B = b;
+
+            Midpoint = (A + B) / 2f;
         }
 
         public static void Log(Edge e)
@@ -18,21 +21,6 @@ namespace Generation.Voronoi {
             #if UNITY_EDITOR
             Debug.Log($"Edge - A: {e.A}, B: {e.B}");
             #endif
-        }
-
-        public sealed class CompareAsSegment : IEqualityComparer<Edge>
-        {
-            public bool Equals(Edge edgeOne, Edge edgeTwo)
-            {
-                return (edgeOne.A == edgeTwo.A && edgeOne.B == edgeTwo.B) ||
-                       (edgeOne.A == edgeTwo.B && edgeOne.B == edgeTwo.A);
-            }
-
-            public int GetHashCode(Edge edge)
-            {
-                return (edge.A.x.GetHashCode() ^ edge.A.y.GetHashCode() << 2 ^ edge.A.z.GetHashCode() >> 2) ^
-                       (edge.B.x.GetHashCode() ^ edge.B.y.GetHashCode() << 2 ^ edge.B.z.GetHashCode() >> 2);
-            }
         }
     }
 }
