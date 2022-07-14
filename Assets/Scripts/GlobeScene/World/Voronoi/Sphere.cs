e9e3d4b3e12bf6933294890b0d5f24a83a60a6db
justin_k_m <justinm0623@gmail.com>
2022-07-14 01:45:52 -0400

    Update Sphere.cs

--- Assets/Scripts/GlobeScene/World/Voronoi/Sphere.cs
+++ Assets/Scripts/GlobeScene/World/Voronoi/Sphere.cs
@@ -12,6 +12,7 @@ namespace Generation.Voronoi
         private float jitter;
         private Vector3[] directions;
         private TimeTracker debugTimeTracker;
+
         public GameObject CubeSphereMesh { get; private set; }
         public GameObject VoronoiSphereMesh { get; private set; }
 
@@ -33,7 +34,7 @@ namespace Generation.Voronoi
 
 
             GetVerticesAndTriangles(out vertices, out triangles);
-            GetVoronoiCells(vertices, triangles);
+            // GetVoronoiCells(vertices, triangles);
             BuildGameObjects(vertices, triangles);
         }
 
@@ -44,14 +45,25 @@ namespace Generation.Voronoi
         {
             if (debugTimeTracker != null) debugTimeTracker.LogTimeStart("GetVerticesAndTriangles()");
 
-            Dictionary<Vector3, List<int>> map = new Dictionary<Vector3, List<int>>();
             List<Vector3> vs = new List<Vector3>();
             List<int> ts = new List<int>();
+
+            List<int>[] borderIndices = new List<int>[20];
+            Dictionary<Vector3, int[]> borderIndicesLocalUp = new Dictionary<Vector3, int[]>();
+
+            for (int a = 0; a < 20; a++) borderIndices[a] = new List<int>();
+
+            borderIndicesLocalUp[Vector3.forward] = new int[] { 8, 9, 10, 11 };
+            borderIndicesLocalUp[Vector3.up] = new int[] { 12, 13, 14, 8 };
+            borderIndicesLocalUp[Vector3.down] = new int[] { 11, 15, 16, 17 };
+            borderIndicesLocalUp[Vector3.left] = new int[] { 13, 18, 9, 15 };
+            borderIndicesLocalUp[Vector3.right] = new int[] { 14, 10, 19, 16 };
+            borderIndicesLocalUp[Vector3.back] = new int[] { 17, 19, 18, 12 };
 
             // Generate Vertices and Triangles
             // For each direction
 
-            int index = 0, uniqueVIndex = 0;
+            int index = 0;
             for (int f = 0; f < directions.Length; f++)
             {
                 Vector3 localUp = directions[f];
@@ -67,17 +79,12 @@ namespace Generation.Voronoi
                         Vector3 pointOnCube = localUp + (percent.x - .5f) * 2f * axisA + (percent.y - .5f) * 2f * axisB;
                         Vector3 pointOnSphere = PointOnCubeToPointOnSphere(pointOnCube);
 
-                        vs.Add(pointOnSphere);
-
-                        // If map already contains the vertex, that means that it is a border vertex, i to the map
-                        if (!map.ContainsKey(pointOnSphere))
+                        if (x == 0 || x == resolution - 1 || y == 0 || y == resolution - 1)
                         {
-                            map.Add(pointOnSphere, new List<int>());
-                            map[pointOnSphere].Add(uniqueVIndex);
-                            uniqueVIndex++;
+                            borderIndices[OrganizeBorderVertex(pointOnSphere, x, y, localUp, borderIndicesLocalUp)].Add(index);
                         }
 
-                        map[pointOnSphere].Add(index);
+                        vs.Add(pointOnSphere);
 
                         // Triangles
                         if (x != resolution - 1 && y != resolution - 1)
@@ -97,48 +104,62 @@ namespace Generation.Voronoi
 
             if (debugTimeTracker != null) debugTimeTracker.LogTimeElapsed("Generated Vertices and Triangles");
 
-            // Collapse Vertices / Triangles
-            // Track both unique vertices, and regular vertices.
-            // Access map using regular vertices, get the unique vertex index associated with it
-            for (int a = 0; a < ts.Count; a++)
-            {
-                ts[a] = map[vs[ts[a]]][0];
-            }
 
-            /*  SLOW
-            int ind = 0;
-            foreach(KeyValuePair<Vector3, List<int>> kvp in map)
-            {
-                // If there is more than one index per vertex, that means that the vertex is shared with another triangle i.e. border vertex.
-                // We need to collapse the vertex (by replacing all the triangles with the first instance of i)
+            HashSet<Vector3> vHash = new HashSet<Vector3>();
+            foreach (Vector3 v in vs) vHash.Add(v);
+            List<Vector3> collapsedVs = vHash.ToList();
 
-                for(int a = 0; a < kvp.Value.Count; a++)
+            // Collapse Triangles
+            foreach(List<int> vsToChange in borderIndices)
+            {
+                if (vsToChange.Count > 0)
                 {
-                    int[] indexes = Enumerable.Range(0, ts.Count).Where(i => ts[i] == kvp.Value[a]).ToArray();
-                    for(int b = 0; b < indexes.Length; b++)
+                    int vertIndex = collapsedVs.IndexOf(vs[vsToChange[0]]);
+                    for (int vIndex = 0; vIndex < vsToChange.Count; vIndex++)
                     {
-                        ts[indexes[b]] = ind;
+                        ts = ts.Select(t => t == vsToChange[vIndex] ? vertIndex : t).ToList();
                     }
                 }
-                ind++;
             }
-            */
 
             if (debugTimeTracker != null) debugTimeTracker.LogTimeElapsed("Collapsed Vertices and Triangles");
 
-            vertices = map.Keys.ToArray();
+            vertices = collapsedVs.ToArray();
             triangles = ts.ToArray();
+
+            Debug.Log(vertices.Length);
+            Debug.Log(ts.IndexOf(Mathf.Max(triangles)));
+            Debug.Log(triangles.Length);
 
             if (debugTimeTracker != null) debugTimeTracker.LogTimeEnd();
         }
 
         private void GetVoronoiCells(Vector3[] vs, int[] ts)
         {
-            // NEEDS IMPROVEMENT
-
             if (debugTimeTracker != null) debugTimeTracker.LogTimeStart("GetVoronoiCells()");
 
-            if (debugTimeTracker != null) debugTimeTracker.LogTimeElapsed("Triangle Objects Generated, Edge Objects Generated");
+            // vertexIndex dictionary
+            Dictionary<Edge, List<Triangle>> dict = new Dictionary<Edge, List<Triangle>>(new CompareAsSegment());
+            List<Triangle> triangles = new List<Triangle>();
+
+            for (int a = 0; a < ts.Length; a += 3)
+            {
+                Triangle triangle = new Triangle(vs[ts[a + 0]], vs[ts[a + 1]], vs[ts[a + 2]]);
+                triangles.Add(triangle);
+
+                Edge A = new Edge(vs[ts[a + 0]], vs[ts[a + 1]]);
+                Edge B = new Edge(vs[ts[a + 1]], vs[ts[a + 2]]);
+                Edge C = new Edge(vs[ts[a + 2]], vs[ts[a + 0]]);
+
+                if (!dict.ContainsKey(A)) dict.Add(A, new List<Triangle>());
+                dict[A].Add(triangle);
+                if (!dict.ContainsKey(B)) dict.Add(B, new List<Triangle>());
+                dict[B].Add(triangle);
+                if (!dict.ContainsKey(C)) dict.Add(C, new List<Triangle>());
+                dict[C].Add(triangle);
+            }
+
+            if (debugTimeTracker != null) debugTimeTracker.LogTimeElapsed("Triangle Objects Generated, Vertex Dictionary Generated");
 
             if (debugTimeTracker != null) debugTimeTracker.LogTimeElapsed("Triangle Neighbors Assigned");
 
@@ -158,6 +179,56 @@ namespace Generation.Voronoi
 
             MeshRenderer meshRenderer = CubeSphereMesh.AddComponent<MeshRenderer>();
             meshRenderer.sharedMaterial = Resources.Load<Material>("Materials/Globe/Map");
+        }
+
+        private int OrganizeBorderVertex(Vector3 v, int x, int y, Vector3 localUp, Dictionary<Vector3,int[]> borderIndicesLocalUp)
+        {
+            // Corner vertices cover indices 0-7, dimensions seen below
+            if (v == PointOnCubeToPointOnSphere(new Vector3(1, -1, 1))) return 0;
+            else if (v == PointOnCubeToPointOnSphere(new Vector3(1, 1, 1))) return 1;
+            else if (v == PointOnCubeToPointOnSphere(new Vector3(-1, -1, 1))) return 2;
+            else if (v == PointOnCubeToPointOnSphere(new Vector3(-1, 1, 1))) return 3;
+            else if (v == PointOnCubeToPointOnSphere(new Vector3(-1, 1, -1))) return 4;
+            else if (v == PointOnCubeToPointOnSphere(new Vector3(1, 1, -1))) return 5;
+            else if (v == PointOnCubeToPointOnSphere(new Vector3(1, -1, -1))) return 6;
+            else if (v == PointOnCubeToPointOnSphere(new Vector3(-1, -1, -1))) return 7;
+
+            // Edge vertices are as follows: 
+            // - forward up     = 8
+            // - forward left   = 9
+            // - forward right  = 10
+            // - forward down   = 11
+            // - up up          = 12
+            // - up left        = 13
+            // - up right       = 14
+            // - up down        = 8
+            // - down up        = 11
+            // - down left      = 15
+            // - down right     = 16
+            // - down down      = 17
+            // - left up        = 13
+            // - left left      = 18
+            // - left right     = 9
+            // - left down      = 15
+            // - right up       = 14
+            // - right left     = 10
+            // - right right    = 19
+            // - right down     = 16
+            // - back up        = 17
+            // - back left      = 19
+            // - back right     = 18
+            // - back down      = 12
+            // Up
+            else if (y == 0) return borderIndicesLocalUp[localUp][0];
+            // Left
+            else if (x == 0) return borderIndicesLocalUp[localUp][1];
+            // Right
+            else if (x == resolution - 1) return borderIndicesLocalUp[localUp][2];
+            // Down
+            else if (y == resolution - 1) return borderIndicesLocalUp[localUp][3];
+
+            // Throw Error
+            return -1;
         }
 
         public static Vector3 PointOnCubeToPointOnSphere(Vector3 p)
