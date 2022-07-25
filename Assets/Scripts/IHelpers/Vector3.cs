adedd288d4183e820a465e3bb36b600295ea1184
justin_k_m <justinm0623@gmail.com>
2022-07-25 02:17:21 -0400

    Update Vector3.cs

--- Assets/Scripts/IHelpers/Vector3.cs
+++ Assets/Scripts/IHelpers/Vector3.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 namespace IHelper
 {
-    public static class Vector3
+    public struct Vector3
     {
         public static UnityEngine.Vector3 Abs(UnityEngine.Vector3 v)
         {
-            return new UnityEngine.Vector3(Mathf.Abs(v.x), Mathf.Abs(v.y), Mathf.Abs(v.z));
+            return new UnityEngine.Vector3(UnityEngine.Mathf.Abs(v.x), UnityEngine.Mathf.Abs(v.y), UnityEngine.Mathf.Abs(v.z));
         }
     }
 }
