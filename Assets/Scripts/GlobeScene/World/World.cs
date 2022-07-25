0eca9c1cb09f852094ff52186089f8123b54ebe0
justin_k_m <justinm0623@gmail.com>
2022-07-25 02:17:03 -0400

    Update World.cs

--- Assets/Scripts/GlobeScene/World/World.cs
+++ Assets/Scripts/GlobeScene/World/World.cs
@@ -4,46 +4,42 @@ using UnityEngine;
 using IDebug;
 using Generation.Voronoi;
 
-public class World : MonoBehaviour
+public class World : BaseObjectMono
 {
-    [Header("Log Time")]
-    [Tooltip("When enabled, will output the times for the given script.")]
+    [Header("Debug")]
+    [Tooltip("When enabled, will output the times for this script.")]
     [SerializeField] private bool logWorld;
-    [SerializeField] private bool logVoronoiSphere;
+
     [Header("VoronoiSphere")]
     [Tooltip("Specifies the resolution of the voronoi sphere initial vertices.")]
     [SerializeField] [Range(2,256)] private int resolution = 2;
     [Tooltip("Adds randomization in points after voronoi cells are calculated.")]
     [SerializeField] [Range(0f, 1f)] private float jitter = 0f;
-
-    private Sphere voronoiSphere;
-    private TimeTracker debugTimeTracker;
-
-    private Color[] color = new Color[] { new Color(0,0,0), new Color(.25f,0,0), new Color(.5f,0,0), new Color(.75f,0,0), new Color(1,0,0),
-                                       new Color(0,.25f,0), new Color(0,.5f,0), new Color(0,.75f,0), new Color(0,1,0), new Color(0,0,.25f),
-                                       new Color(0,0,.5f), new Color(0,0,.75f), new Color(0,0,1), new Color(.25f,.25f,0), new Color(.5f,.25f,0),
-                                       new Color(.75f,.25f,0), new Color(1f,.25f,0), new Color(.25f,.5f,0), new Color(.5f,.5f,0), new Color(.75f,.5f,0)};
+    [Header("- Debug")]
+    [Tooltip("Generates Triangles with unique vertices for triangle debugging.")]
+    [SerializeField] private bool uniqueTriangles;
+    [Tooltip("When enabled, will output the times for the Sphere script.")]
+    [SerializeField] private bool logVoronoiSphere;
 
     private void Start()
     {
         debugTimeTracker = (logWorld) ? new TimeTracker() : null;
-        voronoiSphere = new Sphere(resolution, jitter, logVoronoiSphere);
+        
+        Sphere voronoiSphere;
+        Sphere.DebugProperties sphereDebugProperties;
+
+        GenerateDebugProperties(out sphereDebugProperties);
+
+
+        voronoiSphere = new Sphere(resolution, jitter, sphereDebugProperties);
 
         voronoiSphere.CubeSphereMesh.transform.parent = this.transform;
         // voronoiSphere.VoronoiSphereMesh.transform.parent = this.transform;
     }
 
-    /*
-    private void OnDrawGizmosSelected()
+    private void GenerateDebugProperties(out Sphere.DebugProperties debugSphere)
     {
-        if (Application.isPlaying)
-        {   
-            for(int a = 0; a < voronoiSphere.borderVerts.Length; a++)
-            {
-                Gizmos.color = color[a];
-                foreach(Vector3 vv in voronoiSphere.borderVerts[a]) Gizmos.DrawSphere(vv, 0.05f);
-            }
-        }
+        
+        debugSphere = new Sphere.DebugProperties(uniqueTriangles, logVoronoiSphere);
     }
-    */
 }
