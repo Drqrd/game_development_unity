7b1a8b06b77927a120dd9c74a358927fe230f298
justin_k_m <justinm0623@gmail.com>
2022-09-21 02:07:22 -0400

    Vector3 Helper Functions

--- Assets/Scripts/IHelpers/Vector3.cs
+++ Assets/Scripts/IHelpers/Vector3.cs
@@ -9,6 +9,10 @@ namespace IHelper
             return new UnityEngine.Vector3(UnityEngine.Mathf.Abs(v.x), UnityEngine.Mathf.Abs(v.y), UnityEngine.Mathf.Abs(v.z));
         }
 
+        public static UnityEngine.Vector3 Subtract(UnityEngine.Vector3 v1, UnityEngine.Vector3 v2) {
+            return new UnityEngine.Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+        }
+
         public static UnityEngine.Vector3 Flatten(UnityEngine.Vector3 v, int axis = 2) {
             switch (axis) {
                     case 0:
@@ -21,6 +25,10 @@ namespace IHelper
                         Debug.LogError("Error flattening Vector3. Axis can only be 0,1,2 (x,y,z)");
                         return v;
             }
+        }
+
+        public static bool Approximately(UnityEngine.Vector3 v1, UnityEngine.Vector3 v2, float error = 0.001f) {
+            return Subtract(v1,v2).magnitude <= error;
         }
     }
 }
